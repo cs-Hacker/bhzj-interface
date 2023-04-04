6496f96806f59ce46d94c490779eb33832136993
hasee <cs2584529612@163.com>
2023-04-04 18:57:26 +0800

    first commit

---
+++ src/main/java/com/bhzj/cloud/BhzjInterfaceApplication.java
@@ -0,0 +1,12 @@
+package com.bhzj.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //代表这是一个Spring Boot的启动类
+public class BhzjInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhzjInterfaceApplication.class, args);
+    }
+}
