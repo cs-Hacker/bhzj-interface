4e12cb2916bd79c7247098e481d60ac6fe27d285
hasee <cs2584529612@163.com>
2023-04-06 09:56:57 +0800

    imgUpAndDown

--- src/main/java/com/bhzj/cloud/controller/CommodityController.java
+++ src/main/java/com/bhzj/cloud/controller/CommodityController.java
@@ -1,11 +1,19 @@
 package com.bhzj.cloud.controller;
 
+import com.bhzj.cloud.ConstantConfig;
 import com.bhzj.cloud.common.utils.RestTemplateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController //Controller声明
 @RequestMapping("/commodity")
@@ -14,6 +22,7 @@ public class CommodityController {
     @Autowired
     RestTemplate restTemplate;
 
+    //https://bhzj-interface-41211-8-1317629604.sh.run.tcloudbase.com/image/getImg?imageUrl=d8b2c7c0bff64ec0b2afe6455df1f977.jpeg
     @RequestMapping("/getAllCommodity")
     public String getAllCommodity(){
         return RestTemplateUtil.getResult(restTemplate, "/commodity/getAllCommodity");
@@ -22,16 +31,30 @@ public class CommodityController {
 
 
 
-//    @RequestMapping("addCommodity")
-//    public String addCommodity(HttpServletRequest request,
-//                         @RequestParam("file") MultipartFile myfile,
-//                         @RequestParam("commodityId") String commodityId,
-//                         @RequestParam("commodityName") String commodityName,
-//                         @RequestParam("commodityTypeCode") int commodityTypeCode,
-//                         @RequestParam("commodityType") String commodityType,
-//                         @RequestParam("commodityPrice") String commodityPrice) throws IOException {
-//        return commodityService.addCommodity(request, myfile, commodityId, commodityName, commodityTypeCode, commodityType, commodityPrice);
-//    }
+    @RequestMapping("addCommodity")
+    public String addCommodity(@RequestParam("file") MultipartFile file,
+                               @RequestParam("commodityId") String commodityId,
+                               @RequestParam("commodityName") String commodityName,
+                               @RequestParam("commodityTypeCode") int commodityTypeCode,
+                               @RequestParam("commodityType") String commodityType,
+                               @RequestParam("commodityPrice") String commodityPrice) {
+        HttpHeaders headers = new HttpHeaders();
+        //设置请求类型
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("commodityId", commodityId);
+        params.add("commodityName", commodityName);
+        params.add("commodityTypeCode", commodityTypeCode);
+        params.add("commodityType", commodityType);
+        params.add("commodityPrice", commodityPrice);
+
+        params.add("file", file.getResource());
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+        String saveServiceUrl = ConstantConfig.serviceUri + "/commodity/addCommodity";//http://182.43.106.149:8000
+        ResponseEntity<String> responseEntity = restTemplate.exchange(saveServiceUrl, HttpMethod.POST, requestEntity, String.class);
+        return responseEntity.getBody();
+    }
 
     @RequestMapping("deleteCommodity")
     public String deleteCommodity(
@@ -40,16 +63,33 @@ public class CommodityController {
     }
 
 
-//    @RequestMapping("updateCommodity")
-//    public String updateCommodity(HttpServletRequest request,
-//                               @RequestParam(name="file", required=false) MultipartFile myfile,
-//                               @RequestParam(name="commodityId") String commodityId,
-//                               @RequestParam(name="commodityName", required=false) String commodityName,
-//                               @RequestParam(name="commodityType", required=false) String commodityType,
-//                               @RequestParam(name="commodityPrice", required=false) String commodityPrice,
-//                               @RequestParam(name="updateImg", required=false) String updateImg) throws IOException {
-//        return commodityService.updateCommodity(request, myfile, commodityId, commodityName, commodityType, commodityPrice, updateImg);
-//    }
+    @RequestMapping("updateCommodity")
+    public String updateCommodity(HttpServletRequest request,
+                               @RequestParam(name="file", required=false) MultipartFile file,
+                               @RequestParam(name="commodityId") String commodityId,
+                               @RequestParam(name="commodityName", required=false) String commodityName,
+                               @RequestParam(name="commodityType", required=false) String commodityType,
+                               @RequestParam(name="commodityPrice", required=false) String commodityPrice,
+                               @RequestParam(name="updateImg", required=false) String updateImg) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        //设置请求类型
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("commodityId", commodityId);
+        params.add("commodityName", commodityName);
+        params.add("commodityType", commodityType);
+        params.add("commodityPrice", commodityPrice);
+        params.add("updateImg", updateImg);
+
+        params.add("file", file.getResource());
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+        String saveServiceUrl = ConstantConfig.serviceUri + "/commodity/updateCommodity";//http://182.43.106.149:8000
+        ResponseEntity<String> responseEntity = restTemplate.exchange(saveServiceUrl, HttpMethod.POST, requestEntity, String.class);
+        return responseEntity.getBody();
+
+
+    }
 
     @RequestMapping("/getCommodityListByIds")
     public String getCommodityListByIds(@RequestParam(name="commodityIds") String commodityIds){
--- src/main/java/com/bhzj/cloud/controller/ImageController.java
+++ src/main/java/com/bhzj/cloud/controller/ImageController.java
@@ -1,11 +1,19 @@
 package com.bhzj.cloud.controller;
 
 import com.bhzj.cloud.ConstantConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -15,8 +23,11 @@ import java.net.URL;
 @RequestMapping("/image")
 public class ImageController {
 
-    //http://127.0.0.1:8081/image/getImg?imageUrl=d8b2c7c0bff64ec0b2afe6455df1f977.jpeg
-    @RequestMapping(path = "getImg")
+    @Autowired
+    RestTemplate restTemplate;
+
+    //https://bhzj-interface-41211-8-1317629604.sh.run.tcloudbase.com/image/getImg?imageUrl=d8b2c7c0bff64ec0b2afe6455df1f977.jpeg
+    @RequestMapping(path = "/getImg")
     public void getImg(@RequestParam("imageUrl") String imageUrl, HttpServletResponse response) throws IOException {//
 
         String path = ConstantConfig.serviceUriImages + imageUrl;
@@ -32,5 +43,38 @@ public class ImageController {
 //        ImageIO.write(img, "jpeg", response.getOutputStream());//png
 //        System.out.println("--------");
     }
+
+
+
+
+//    @RequestMapping("/upload/logFiles")
+//    public String receiveFile(HttpServletRequest request) {
+//        //获取上传文件列表
+//        MultiValueMap<String, MultipartFile> multiFileMap = ((MultipartHttpServletRequest) request).getMultiFileMap();
+//        //获取文件列表第的第一个文件key
+//        String first = (String) multiFileMap.keySet().iterator().next();
+//        //获取文件列表中第一个文件
+//        MultipartFile file = (MultipartFile) multiFileMap.getFirst(first);
+//        HttpHeaders headers = new HttpHeaders();
+//        //设置请求类型
+//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+//        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+//        params.add("vin", vin);
+//        params.add("Command_session_ID", cmdSessionIdStr);
+//        params.add("progress", progress);
+//        params.add("sig", sig);
+//        params.add("iv", iv);
+//        //将获取的MultipartFile文件放入HttpEntity中
+//        params.add("file", file.getResource());
+//        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+//        ResponseEntity<String> responseEntity = restTemplate.exchange(logUpLoadUrl, HttpMethod.POST, requestEntity, String.class);
+//        return responseEntity.getBody();
+//    }
+
+
+
+
+
+
 
 }
