5f8b6c1f053bcaff3fd5afeccdb35074939ee0cb
hasee <cs2584529612@163.com>
2023-04-26 11:52:35 +0800

    commodityType

--- src/main/java/com/bhzj/cloud/ConstantConfig.java
+++ src/main/java/com/bhzj/cloud/ConstantConfig.java
@@ -18,7 +18,18 @@ public class ConstantConfig {
         cTNameCTCodeMap.put("肉蛋家禽", 2);
         cTNameCTCodeMap.put("休闲零食", 3);
         cTNameCTCodeMap.put("牛奶酸奶", 4);
-    }
 
+        cTNameCTCodeMap.put("熟食面点", 5);
+        cTNameCTCodeMap.put("方便速食", 6);
+        cTNameCTCodeMap.put("酒水饮料", 7);
+        cTNameCTCodeMap.put("美妆个护", 8);
+        cTNameCTCodeMap.put("冷藏冷冻", 9);
+        cTNameCTCodeMap.put("粮油调味", 10);
+        cTNameCTCodeMap.put("纸品家清", 11);
+        cTNameCTCodeMap.put("日用百货", 12);
+        cTNameCTCodeMap.put("母婴呵护", 13);
+        cTNameCTCodeMap.put("成人用品", 14);
+
+    }
 
 }
