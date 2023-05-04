3f3d8a7f917862d46dbe53b394911d663f814148
hasee <cs2584529612@163.com>
2023-05-04 18:07:49 +0800

    CTCode

--- src/main/java/com/bhzj/cloud/ConstantConfig.java
+++ src/main/java/com/bhzj/cloud/ConstantConfig.java
@@ -13,22 +13,22 @@ public class ConstantConfig {
 
     public static Map<String, Integer> cTNameCTCodeMap = new HashMap();
     static {
-        cTNameCTCodeMap.put("时令蔬菜", 0);
-        cTNameCTCodeMap.put("新鲜水果", 1);
-        cTNameCTCodeMap.put("肉蛋家禽", 2);
-        cTNameCTCodeMap.put("休闲零食", 3);
-        cTNameCTCodeMap.put("牛奶酸奶", 4);
+        cTNameCTCodeMap.put("时令蔬菜", 1);
+        cTNameCTCodeMap.put("新鲜水果", 2);
+        cTNameCTCodeMap.put("肉蛋家禽", 3);
+        cTNameCTCodeMap.put("休闲零食", 4);
+        cTNameCTCodeMap.put("牛奶酸奶", 5);
 
-        cTNameCTCodeMap.put("熟食面点", 5);
-        cTNameCTCodeMap.put("方便速食", 6);
-        cTNameCTCodeMap.put("酒水饮料", 7);
-        cTNameCTCodeMap.put("美妆个护", 8);
-        cTNameCTCodeMap.put("冷藏冷冻", 9);
-        cTNameCTCodeMap.put("粮油调味", 10);
-        cTNameCTCodeMap.put("纸品家清", 11);
-        cTNameCTCodeMap.put("日用百货", 12);
-        cTNameCTCodeMap.put("母婴呵护", 13);
-        cTNameCTCodeMap.put("成人用品", 14);
+        cTNameCTCodeMap.put("熟食面点", 6);
+        cTNameCTCodeMap.put("方便速食", 7);
+        cTNameCTCodeMap.put("酒水饮料", 8);
+        cTNameCTCodeMap.put("美妆个护", 9);
+        cTNameCTCodeMap.put("冷藏冷冻", 10);
+        cTNameCTCodeMap.put("粮油调味", 11);
+        cTNameCTCodeMap.put("纸品家清", 12);
+        cTNameCTCodeMap.put("日用百货", 13);
+        cTNameCTCodeMap.put("母婴呵护", 14);
+        cTNameCTCodeMap.put("成人用品", 15);
 
     }
 
